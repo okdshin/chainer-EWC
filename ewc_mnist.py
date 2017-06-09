7c129228f5a8cec8121e5e4cef7761a59b5b4d34
okdshin <kokuzen@gmail.com>
2017-06-09 13:15:25 +0900

    simple experiment

--- ewc_mnist.py
+++ ewc_mnist.py
@@ -1,13 +1,17 @@
+import os
+import argparse
+import datetime
 import numpy as np
 import chainer
-from chainer import cuda, Function, gradient_check, report, training, utils, Variable
-from chainer import dataset, datasets, iterators, optimizers, serializers
-from chainer import Link, Chain, ChainList
+from chainer import cuda, Function, gradient_check, \
+                    report, training, utils, Variable
+from chainer import datasets, iterators, optimizers, serializers
+from chainer import Link, Chain
 import chainer.functions as F
 import chainer.links as L
 from chainer.training import extensions
+from chainer import reporter
 
-from sklearn import preprocessing
 
 class MLP(Chain):
     def __init__(self, hidden_num, out_num):
@@ -35,6 +39,8 @@ def make_variable_list(model):
 
 
 class EWC(Chain):
+    compute_accuracy = True
+
     def __init__(self, predictor, lam, num_samples):
         super(EWC, self).__init__()
         with self.init_scope():
@@ -44,10 +50,17 @@ class EWC(Chain):
         self.stored_variable_list = None
         self.lam = lam
         self.num_samples = num_samples
+        self.y = None
+        self.loss = None
+        self.accuracy = None
 
-    def __call__(self, x, t):
-        self.y = self.predictor(x)
-        self.accuracy = F.accuracy(self.y, t)
+    def __call__(self, *args):
+        x = args[:-1]
+        t = args[-1]
+        self.y = None
+        self.loss = None
+        self.accuracy = None
+        self.y = self.predictor(*x)
         self.loss = F.softmax_cross_entropy(self.y, t)
 
         if self.stored_variable_list is not None and \
@@ -57,7 +70,10 @@ class EWC(Chain):
                         self.fisher_list[i] *
                         F.square(self.variable_list[i][1] -
                                  self.stored_variable_list[i]))
-
+        reporter.report({'loss': self.loss}, self)
+        if self.compute_accuracy:
+            self.accuracy = F.accuracy(self.y, t)
+            reporter.report({'accuracy': self.accuracy}, self)
         return self.loss
 
     def compute_fisher(self, dataset):
@@ -86,45 +102,32 @@ class EWC(Chain):
             self.stored_variable_list.append(np.copy(var[1].data))
 
 
-def train_task(model, epoch_num, train_dataset, test_dataset_list, batch_size):
+def train_task(args, train_name, model, epoch_num,
+               train_dataset, test_dataset_dict, batch_size):
     optimizer = optimizers.SGD()
     optimizer.setup(model)
 
     train_iter = iterators.SerialIterator(train_dataset, batch_size)
-    test_iter_list = [iterators.SerialIterator(
+    test_iter_dict = {name: iterators.SerialIterator(
             test_dataset, batch_size, repeat=False, shuffle=False)
-            for test_dataset in test_dataset_list]
+            for name, test_dataset in test_dataset_dict.items()}
 
-    accuracy_sum = 0
-    loss_sum = 0
-    while train_iter.epoch != epoch_num:
-        batch = train_iter.next()
-        xs, ts = dataset.convert.concat_examples(batch)
-        optimizer.update(model, xs, ts)
-        loss_sum += float(model.loss.data) * len(ts.data)
-        accuracy_sum += float(model.accuracy.data) * len(ts.data)
-
-        if train_iter.is_new_epoch:
-            print("epoch:", train_iter.epoch)
-            print("train mean loss: {}, accuracy: {}".format(
-                loss_sum / len(train_dataset),
-                accuracy_sum / len(train_dataset)))
-            loss_sum = 0
-            accuracy_sum = 0
-
-            # evaluation
-            for test_dataset, test_iter in zip(test_dataset_list, test_iter_list):
-                test_accuracy_sum = 0
-                test_loss_sum = 0
-                for batch in test_iter:
-                    xs, ts = dataset.convert.concat_examples(batch)
-                    model(xs, ts)
-                    test_loss_sum += float(model.loss.data) * len(ts.data)
-                    test_accuracy_sum += float(model.accuracy.data) * len(ts.data)
-                test_iter.reset()
-                print("test mean loss: {}, accuracy: {}".format(
-                    test_loss_sum / len(test_dataset),
-                    test_accuracy_sum / len(test_dataset)))
+    updater = training.StandardUpdater(train_iter, optimizer)
+    trainer = training.Trainer(updater, (epoch_num, 'epoch'), out=args.out)
+    for name, test_iter in test_iter_dict.items():
+        trainer.extend(extensions.Evaluator(test_iter, model), name)
+    trainer.extend(extensions.LogReport())
+    trainer.extend(extensions.PrintReport(
+        ['epoch', 'main/loss'] +
+        [test+'/main/loss' for test in test_dataset_dict.keys()] +
+        ['main/accuracy'] +
+        [test+'/main/accuracy' for test in test_dataset_dict.keys()]))
+    trainer.extend(extensions.ProgressBar())
+    trainer.extend(extensions.PlotReport(
+        [test+"/main/accuracy" for test
+         in test_dataset_dict.keys()],
+        file_name=train_name+".png"))
+    trainer.run()
 
 
 def permutate_mnist(dataset_list):
@@ -139,34 +142,69 @@ def permutate_mnist(dataset_list):
     return (permutate_mnist_aux(dataset) for dataset in dataset_list)
 
 
-def main():
-    epoch_num = 800
-    batch_size = 128
-    hidden_num = 400
-    output_dim = 10
-    lam = 15
-    num_samples = 400
-    model = EWC(MLP(hidden_num, output_dim), lam, num_samples)
+def train_tasks_continuosly(
+        args, model, train, test, train2, test2, enable_ewc):
+    # Train Task A or load trained model
+    if os.path.exists("mlp_taskA.model") or args.skip_taskA:
+        print("load taskA model")
+        serializers.load_npz("./model50/mlp_taskA.model", model)
+    else:
+        print("train taskA")
+        train_task(args, "train_task_a"+("_with_ewc" if enable_ewc else ""),
+                   model, args.epoch, train,
+                   {"TaskA": test}, args.batchsize)
+        print("save the model")
+        serializers.save_npz("mlp_taskA.model", model)
 
-    # Train Task A
-    #train, test = chainer.datasets.get_mnist()
-    train, test = chainer.datasets.get_cifar10()
-    train_task(model, epoch_num, train, [test], batch_size)
-    print("save the model")
-    serializers.save_npz("mlp_taskA.model", model)
-    #serializers.load_npz("model50/mlp_taskA.model", model)
-
-    model.compute_fisher(train)
-    model.store_variables()
+    if enable_ewc:
+        print("enable EWC")
+        model.compute_fisher(train)
+        model.store_variables()
 
     # Train Task B
-    train2, test2 = permutate_mnist([train, test])
-    train_task(model, epoch_num, train2, [test2, test], batch_size)
+    print("train taskB")
+    train_task(args, "train_task_ab"+("_with_ewc" if enable_ewc else ""),
+               model, args.epoch, train2,
+               {"TaskA": test, "TaskB": test2}, args.batchsize)
     print("save the model")
-    serializers.save_npz("mlp_taskAB.model", model)
-    #serializers.load_npz("model50/mlp_taskAB.model", model)
+    serializers.save_npz(
+            "mlp_taskAB"+("_with_ewc" if enable_ewc else "")+".model", model)
 
-    #train_task(model, epoch_num, train2, [test2, test], batch_size)
+
+def main():
+    output_dim = 10
+
+    parser = argparse.ArgumentParser(description='EWC MNIST')
+    parser.add_argument('--batchsize', '-b', type=int, default=100,
+                        help='Number of images in each mini-batch')
+    parser.add_argument('--epoch', '-e', type=int, default=800,
+                        help='Number of sweeps over the dataset to train')
+    parser.add_argument('--gpu', '-g', type=int, default=-1,
+                        help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--out', '-o', default='result',
+                        help='Directory to output the result')
+    parser.add_argument('--lam', '-l', type=float, default=15.,
+                        help='lambda parameter for EWC loss')
+    parser.add_argument('--num_samples', '-n', type=int, default=200,
+                        help='number of samples to compute fisher')
+    parser.add_argument('--hidden', '-hi', type=int, default=50,
+                        help='number of hidden node')
+    parser.add_argument('--skip_taskA', '-s', type=bool, default=False,
+                        help='whether skip training taskA or not')
+    args = parser.parse_args()
+
+    model = EWC(MLP(args.hidden, output_dim), args.lam, args.num_samples)
+
+    train, test = chainer.datasets.get_mnist()
+    train2, test2 = permutate_mnist([train, test])
+
+    print("Train without EWC")
+    train_tasks_continuosly(args, model, train, test, train2, test2,
+                            enable_ewc=False)
+
+    print("Train with EWC")
+    train_tasks_continuosly(args, model, train, test, train2, test2,
+                            enable_ewc=True)
 
 
 if __name__ == "__main__":
